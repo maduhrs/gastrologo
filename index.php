da588320d7af7270e67d3cfe1f68839378f86c66
maduhrs <dulipe2105@gmail.com>
2020-10-15 09:15:14 -0300

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>  Gastrologo </title>
+</head>
+<body>
+<h1> ob_iconv_handler  --glollplp</h1>
+
+</body>
+</html>
+
